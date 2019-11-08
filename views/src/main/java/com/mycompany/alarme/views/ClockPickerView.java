60c4f3c955ce9aaad62becfdc001289283bb00ac
Nazar Pinonzhyk <nz2develop@gmail.com>
2019-11-08 19:12:32 +0200

    Add basic clock picker attributes

--- views/src/main/java/com/mycompany/alarme/views/ClockPickerView.java
+++ views/src/main/java/com/mycompany/alarme/views/ClockPickerView.java
@@ -1,6 +1,7 @@
 package com.mycompany.alarme.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,6 +10,7 @@ import android.graphics.SweepGradient;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.FloatRange;
 import androidx.annotation.IntRange;
 import androidx.annotation.Nullable;
@@ -16,22 +18,31 @@ import androidx.core.math.MathUtils;
 
 public class ClockPickerView extends View {
 
+    public static final float ARC_STROKE_DEFAULT_VALUE = 0.05f;
+    public static final int SEGMENT_DIVISION_DEFAULT_LINE_SIZE = 20;
+    public static final int SEGMENT_DIVISION_DEFAULT_OFFSET = 50;
+    public static final int SEGMENT_DIVISION_DEFAULT_LINE_WIDTH = 5;
+
     private Paint debugPaint;
 
     private Paint segmentDivisionPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private double segmentDivisionLineSize = 20;
+    private double segmentDivisionLineSize; // todo convert to percentage value
+    private @ColorInt int segmentDivisionLineColor;
     private int segmentsCount = 12;
     private int segmentDegree = 360 / segmentsCount;
-    private int segmentDivisionOffset = 50;
+    private int segmentDivisionOffset;
     private int segmentDivisionEndRadius;
+    private int segmentDivisionLineWidth;
 
-    private Paint rangeArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private float rangeArchStrokeWidthPercentageOfRadius = 0.05f;
-    private RectF rangeArcRect = new RectF();
+    private RectF arcRect = new RectF();
+    private float arcStrokeValue;
     private int gradientStartDegree;
     private float gradientRange;
-
+    private Paint rangeArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private @ColorInt int rangeArcStartColor;
+    private @ColorInt int rangeArcEndColor;
     private Paint boundsArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private @ColorInt int boundsArcColor;
 
     private int radius;
     private int startX;
@@ -39,20 +50,34 @@ public class ClockPickerView extends View {
 
     public ClockPickerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.ClockPickerView);
+        arcStrokeValue = typedArray.getFloat(R.styleable.ClockPickerView_clock_arcSize, ARC_STROKE_DEFAULT_VALUE);
+        rangeArcStartColor = typedArray.getColor(R.styleable.ClockPickerView_clock_rangeArcStartColor, Color.BLUE);
+        rangeArcEndColor = typedArray.getColor(R.styleable.ClockPickerView_clock_rangeArcEndColor, Color.YELLOW);
+        boundsArcColor = typedArray.getColor(R.styleable.ClockPickerView_clock_boundsArcColor, Color.GRAY);
+        segmentDivisionLineSize = typedArray.getDimensionPixelSize(R.styleable.ClockPickerView_clock_segmentsDivisionLineSize, SEGMENT_DIVISION_DEFAULT_LINE_SIZE);
+        segmentDivisionOffset = typedArray.getInt(R.styleable.ClockPickerView_clock_segmentsDivisionOffset, SEGMENT_DIVISION_DEFAULT_OFFSET);
+        segmentDivisionLineColor = typedArray.getColor(R.styleable.ClockPickerView_clock_segmentsDivisionLineColor, Color.GRAY);
+        segmentDivisionLineWidth = typedArray.getInt(R.styleable.ClockPickerView_clock_segmentsDivisionLineWidth, SEGMENT_DIVISION_DEFAULT_LINE_WIDTH);
+        typedArray.recycle();
+
         rangeArcPaint.setStyle(Paint.Style.STROKE);
 
         boundsArcPaint.setStyle(Paint.Style.STROKE);
-        boundsArcPaint.setColor(Color.GRAY);
+        boundsArcPaint.setColor(boundsArcColor);
 
         segmentDivisionPaint.setStyle(Paint.Style.STROKE);
-        segmentDivisionPaint.setColor(Color.BLACK);
-        segmentDivisionPaint.setStrokeWidth(5);
+        segmentDivisionPaint.setColor(segmentDivisionLineColor);
+        segmentDivisionPaint.setStrokeWidth(segmentDivisionLineWidth);
 
         debugPaint = new Paint();
         debugPaint.setStyle(Paint.Style.STROKE);
         debugPaint.setColor(Color.BLACK);
 
-        setupGradientShader(0.4f, 270);
+        updateCirclePaintShader();
+        if (isInEditMode()) {
+            setupGradientShader(0.4f, 270);
+        }
     }
 
     public void setupGradientShader(@FloatRange(from = 0f, to = 1f) float range, @IntRange(from = 0, to = 360) int gradientStart) {
@@ -75,13 +100,13 @@ public class ClockPickerView extends View {
         startY = h / 2;
         radius = Math.min(w, h) / 2;
 
-        final int rangeArcStrokeWidth = ((int) (radius * rangeArchStrokeWidthPercentageOfRadius));
+        final int rangeArcStrokeWidth = ((int) (radius * arcStrokeValue));
         rangeArcPaint.setStrokeWidth(rangeArcStrokeWidth);
         boundsArcPaint.setStrokeWidth(rangeArcStrokeWidth);
 
         final float halfOfRangeArchStrokeWidget = rangeArcStrokeWidth / 2f;
         // set the circle rect for gradient arc, shrink it by a half of stroke width in order to fit inside view bounds
-        rangeArcRect.set(startX - (radius - halfOfRangeArchStrokeWidget),
+        arcRect.set(startX - (radius - halfOfRangeArchStrokeWidget),
                 startY - (radius - halfOfRangeArchStrokeWidget),
                 startX + (radius - halfOfRangeArchStrokeWidget),
                 startY + (radius - halfOfRangeArchStrokeWidget));
@@ -93,7 +118,7 @@ public class ClockPickerView extends View {
 
     private void updateCirclePaintShader() {
         rangeArcPaint.setShader(new SweepGradient(startX, startY, new int[]{
-                Color.BLUE, Color.YELLOW
+                rangeArcStartColor, rangeArcEndColor
         }, new float[]{
                 0.0f, gradientRange
         }));
@@ -120,9 +145,9 @@ public class ClockPickerView extends View {
 
         final float rangeSweepAngle = 360 * gradientRange;
         canvas.rotate(gradientStartDegree, startX, startY);
-        canvas.drawArc(rangeArcRect, 0, rangeSweepAngle, false, rangeArcPaint);
+        canvas.drawArc(arcRect, 0, rangeSweepAngle, false, rangeArcPaint);
 
-        canvas.drawArc(rangeArcRect, rangeSweepAngle, 360 - rangeSweepAngle, false, boundsArcPaint);
+        canvas.drawArc(arcRect, rangeSweepAngle, 360 - rangeSweepAngle, false, boundsArcPaint);
     }
 
 }
